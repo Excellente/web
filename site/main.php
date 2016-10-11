76cc545465ddcba1565b64c4c6441a4ecddabd18
Excellent MSIMANGO <pcheerza@gmail.com>
2016-10-11 18:27:29 +0200

    automated push

--- site/main.php
+++ site/main.php
@@ -8,9 +8,9 @@
 	<body>
 		<div id="header">| Home |</div>
 		<div id="wrapper">
-			<video id="video" width="500" height="400"></video>
+			<!--<video id="video" width="500" height="400"></video>
 			<a href="#" class="btn" id="snapshot">Take Snapshot</a>
-			<canvas id="canvas" width="500" height="400"></canvas>
+			<canvas id="canvas" width="500" height="400"></canvas>-->
 			<a href="logout.php" class="btn">Logout</a>
 		</div>
 		<!--<script src="js/main.js"></script>-->
--- site/verify.php
+++ site/verify.php
@@ -1,20 +1,27 @@
 <?php
 session_start();
 
-$to      = "pcheerza@gmail.com"; //$_SESSION['email'];
-$subject = 'Signup | Verification'; // Give the email a subject
-$message = '
+$to      = "zamanird@gmail.com"; //$_SESSION['email'];
+$subject = 'Verification'; // Give the email a subject
+$message = 'hello';
 
-Thanks for signing up!
-Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.
+$error_report = mail($to, $subject, $message);
+if ($error_report === true)
+  echo "email was successfuly sent". PHP_EOL;
+else {
+  echo "there was an error in sending the email";
+}
+//header("Location: login.php");
+/*
+//$headers = 'From:noreply@yourwebsite.com' . "\r\n";
+Thanks for signing up!'."\r\n".'
+Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.'."\r\n".'
 
-------------------------
-Username: '.$name.'
-------------------------
+------------------------'."\r\n".'
+Username: '.$name.''."\r\n".'
+------------------------'."\r\n".'
 
-Please click this link to activate your account:
-http://www.aremac.co.za/emsimang/verify.php?email='.$email;
-$headers = 'From:noreply@yourwebsite.com' . "\r\n";
-mail($to, $subject, $message, $headers);
-header("Location: login.php");
+Please click this link to activate your account:'."\r\n".'
+http://www.aremac.co.za/emsimang/verify.php?email='.$email."\r\n
+*/
 ?>
